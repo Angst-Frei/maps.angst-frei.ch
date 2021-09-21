9ebe22c681b0613afc28ea29002f51bb97c262fc
Jörg Roth <info@leylines.ch>
2021-09-21 15:34:45 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1cXAET4iMKOSOzuQRpmYQZwTxHMwLCV93MfGNZk1MNpI/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1PRZp5jzotF3LQhc_JCgON2oM2oxWCv_6hr6z_Zlv8LY/edit?usp=sharing';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
