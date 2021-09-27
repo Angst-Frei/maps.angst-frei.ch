7c1a5db36bc887a9f9b96425bcb2bd67c6ecc266
Joerg Roth <joerg.roth@geo.uzh.ch>
2021-09-27 15:58:49 +0200

    update url

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSOwx7faLlCwco-_F8f8CPWCUuPW7NuxdLvYdX_D-axem2t5PU22fDdDvriTY8QTMriG1u3U_Dk121Z/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1PRZp5jzotF3LQhc_JCgON2oM2oxWCv_6hr6z_Zlv8LY/edit#gid=164271551'
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
