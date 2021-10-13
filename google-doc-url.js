299f2589a4c716ef1da58b5ad97fc75749ec7049
Joerg Roth <joerg.roth@geo.uzh.ch>
2021-10-13 16:50:15 +0200

    update 20211013

--- google-doc-url.js
+++
@@ -1,5 +0,0 @@
-// paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1PRZp5jzotF3LQhc_JCgON2oM2oxWCv_6hr6z_Zlv8LY/edit#gid=164271551';
-
-// insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyA5IUHNyPZvWuCMTqvI4DJ2ygqDO5_t5Is';
